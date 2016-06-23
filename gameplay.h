031402d179676dbabb8a021d0873d5d63951bc98
ewler <ewler.ar@gmail.com>
2016-06-22 21:39:53 -0300

    Add files via upload

---
+++ gameplay.h
@@ -0,0 +1,537 @@
+#pragma once
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
+#include "loaders_and_effects.h"
+/*
+typedef struct _melee_damager
+{
+	// Textura que contera o sprite e informaces da textura
+	SDL_Texture* sprite;
+	SDL_Rect snipRect;
+	SDL_Rect presentedRect;
+	
+	// Efeitos sonoros quando for acertado e quando morre
+	Mix_Chunk* gotHit;
+	Mix_Chunk* deathSound;
+	
+	// Pontos de vida, velocidade com que se move e dano causado por contato fisico
+	int healthPoints;
+	float speed;
+	int damage;
+} MeleeDamager;
+*/
+
+
+typedef struct _shooter
+{
+	SDL_Texture* sprite;
+	SDL_Rect snipRect;
+	SDL_Rect presentedRect;
+	
+	Mix_Chunk* gotHitSound;
+	Mix_Chunk* deathSound;
+
+	int healthPoints;
+	float speed;
+	int damage;
+	
+	// Tipo de projetil que o personagem atira
+	//MagicProjectile type;
+} Shooter;
+
+int i = 0;
+int a = 0;
+
+typedef struct _magic_projectile
+{
+	SDL_Texture* sprite;
+	SDL_Rect snipRect;
+	SDL_Rect presentedRect;
+	
+	int activate;
+	int direction;
+	// Efeitos sonoros quando uma parede e acertada e quando um personagem e acertado
+	Mix_Chunk* hitWall;
+	Mix_Chunk* hitCharacter;
+	
+	int damage;
+	float speed;
+} MagicProjectile;
+
+
+
+typedef struct _item
+{
+	SDL_Texture* sprite;
+	SDL_Rect snipRect;
+	SDL_Rect presentedRect;
+	
+	// Efeito sonoro de quando e coletado
+	Mix_Chunk* collectedSound;	
+} Item;
+
+
+typedef struct _collectible
+{
+	SDL_Texture* sprite;
+	SDL_Rect snipRect;
+	SDL_Rect presentedRect;
+	
+	Mix_Chunk* collectedSound
+} Collectible;
+
+
+typedef struct _map
+{
+	// Textura do mapa
+	SDL_Texture* texture;
+	SDL_Rect snipRect;
+} Map;
+
+int tiro();
+
+int gameplay_logic( SDL_Window* l_window, SDL_Renderer* l_renderer )
+{
+	// Flag para trocar o estado do jogo
+	int l_gameState= 3;
+	
+	// Personagens que atiram coisas (inclui o jogador)
+	Shooter player;
+
+		
+	
+	//Shooter beholder;
+	//Shooter necromancer;
+	//Shooter demonBoss;
+/*
+	// Tipos de projeteis/tiros
+	//MagicProjectile arcaneMissile;
+	MagicProjectile fireBall;
+	//MagicProjectile iceSpear;
+	//MagicProjectile laserBeam;
+	//MagicProjectile ghastlyShooting;
+	//MagicProjectile demonicBolt;
+
+	// Personagens que causam dano somente atraves do toque
+	MeleeDamager skeleton;
+	//MeleeDamager gatekeeper;
+	
+	// Itens que modificam o jogador
+	//Item blueRobe;
+	//Item redRobe;
+	//Item greenRobe;
+	
+	// Tipos de colecionaveis (geram score)
+	Collectible greenCrystal;
+	//Collectible blueCrystal;
+	//Collectible redCrystal;
+*/	
+	
+	// Mapas do jogo
+	Map firstMap;
+	//Map lastMap;
+	
+	// Informacoes do player no estado incicial
+	player.sprite = load_texture( "images/sprites/sombra100-60.png", l_window, l_renderer );
+	player.snipRect.x = 0;
+	player.snipRect.y = 0;
+    player.snipRect.w = 80;
+    player.snipRect.h = 80;
+	player.presentedRect.x = 800/2;
+	player.presentedRect.y = 600/2;
+	player.presentedRect.w = 80;
+	player.presentedRect.h = 80; 
+	//player.gotHit = load_sfx( "?" );
+	//player.deathSound = load_sfx( "?" );
+	//player.healthPoints = 100;
+	player.speed = 5.0;
+	//player.damage = 0;
+	
+	MagicProjectile fireBall;
+	MagicProjectile V[50];
+
+
+	for(a=0;a<50;a++)
+	{
+	
+	V[a].snipRect.x = 0;
+	V[a].snipRect.y = 0;
+	V[a].snipRect.h = 30;
+	V[a].snipRect.w = 30;
+	V[a].presentedRect.x = 900;
+	V[a].presentedRect.y = 900; 
+	V[a].presentedRect.w = 30;
+	V[a].presentedRect.h = 30; 
+	
+	}
+
+	// Informacoes do projetil
+	fireBall.sprite = load_texture( "images/sprites/fireball30x30.png", l_window, l_renderer );
+	//fireBall.hitWall = load_sfx( "?" );
+	//fireBall.hitCharacter = load_sfx( "?" );
+	fireBall.damage = 50;
+	fireBall.speed = 2.0;
+/*	
+	//  Informacoes do inimigo
+	skeleton.sprite = load_texture( "images/sprites/esqueleto_80x80.png", l_window, l_renderer );
+	//skeleton.gotHit = load_sfx( "?" );
+	//skeleton.deathSound = load_sfx( "?" );
+	skeleton.healthPoints = 100;
+	skeleton.speed = 3.5;
+	skeleton.damage = 30;
+	
+	// Informacoes do colecionavel
+	greenCrystal.sprite = load_texture( "images/sprites/green_crystal.png", l_window, l_renderer );
+	//greenCrystal.collectedSound = load_sfx( "?" );
+*/	
+	// Informacoes sobre o mapa
+	firstMap.texture = load_texture( "images/levels/4_doors.png", l_window, l_renderer );
+
+
+	SDL_RenderCopy( l_renderer, firstMap.texture, NULL, NULL );
+	SDL_RenderCopy( l_renderer, player.sprite, &player.snipRect, &player.presentedRect );
+	//SDL_RenderCopy( l_renderer, skeleton.sprite, &skeleton.snipRect, &skeleton.presentedRect );
+	SDL_RenderPresent( l_renderer );    
+	
+	// Cuidador de evento
+    SDL_Event e;
+    
+	const Uint8 *keystates = SDL_GetKeyboardState(NULL);
+/*
+	// Definindo teclas para atirar
+	int keyUpIsDown;
+	int keyDownIsDown;
+	int keyLeftIsDown;
+	int keyRightIsDown;
+*/
+	// Definindo teclas para movimentar-se	
+	int keyWIsDown;
+	int keySIsDown;
+	int keyAIsDown;
+	int keyDIsDown;
+	int keyZIsDown;
+	int keyUPIsDown;
+	int keyDOWNIsDown;
+	int keyLEFTIsDown;
+	int keyRIGHTIsDown;
+
+
+	while( l_gameState == 3 )
+	{
+
+		SDL_RenderClear( l_renderer );		
+		SDL_RenderCopy( l_renderer, firstMap.texture, NULL, NULL );
+		SDL_RenderCopy( l_renderer, player.sprite, &player.snipRect, &player.presentedRect );
+        	
+        	for(a=0; a<50; a++)
+			{	
+				if(V[a].presentedRect.y <= 0 || V[a].presentedRect.x <= 0 || V[a].presentedRect.x > 800 || V[a].presentedRect.y > 800 )
+				{
+					V[a].activate = 0;
+				}					
+				if(V[a].activate == 1){
+					switch( V[a].direction )
+					{
+						case 1:
+
+							V[a].presentedRect.y -= fireBall.speed;
+							break;
+
+						case 2:
+
+							V[a].presentedRect.y += fireBall.speed;
+							break;
+
+						case 3:
+
+							V[a].presentedRect.x -= fireBall.speed;
+							break;
+
+						case 4:
+
+							V[a].presentedRect.x += fireBall.speed;
+							break;
+
+						case 5:
+
+							V[a].presentedRect.y -= fireBall.speed;
+							V[a].presentedRect.x -= fireBall.speed;
+							break;
+
+						case 6:
+
+							V[a].presentedRect.y -= fireBall.speed;
+							V[a].presentedRect.x += fireBall.speed;
+							break;
+
+						case 7:
+
+							V[a].presentedRect.y += fireBall.speed;
+							V[a].presentedRect.x -= fireBall.speed;
+							break;
+
+						case 8:
+
+							V[a].presentedRect.y += fireBall.speed;
+							V[a].presentedRect.x += fireBall.speed;
+							break;
+					}
+					SDL_RenderCopy( l_renderer, fireBall.sprite, &V[a].snipRect, &V[a].presentedRect );
+				}
+			}
+        	
+        SDL_RenderPresent( l_renderer );
+
+		while( SDL_PollEvent( &e ) != 0 )
+		{
+
+			if( e.type == SDL_QUIT )
+            {
+                l_gameState = -1;
+            }
+
+    		if( e.type == SDL_KEYDOWN )
+			{
+				// Definindo teclas com o scancode
+				keyWIsDown = keystates[SDL_SCANCODE_W];
+				keySIsDown = keystates[SDL_SCANCODE_S];
+				keyAIsDown = keystates[SDL_SCANCODE_A];
+				keyDIsDown = keystates[SDL_SCANCODE_D];
+				keyUPIsDown = keystates[SDL_SCANCODE_UP];
+				keyDOWNIsDown = keystates[SDL_SCANCODE_DOWN];
+				keyLEFTIsDown = keystates[SDL_SCANCODE_LEFT];
+				keyRIGHTIsDown = keystates[SDL_SCANCODE_RIGHT];
+
+				if( keyWIsDown && keyDIsDown )
+				{
+					player.presentedRect.x += player.speed;
+					player.presentedRect.y -= player.speed;
+					player.snipRect.x -= 100;
+					if( player.snipRect.x < 200 || player.snipRect.x > 300 )
+	    			{
+	    				player.snipRect.x = 200;
+					}
+				} 
+				else if( keyWIsDown && keyAIsDown )
+				{
+					player.presentedRect.x -= player.speed;
+					player.presentedRect.y -= player.speed;
+					player.snipRect.x -= 100;
+					if( player.snipRect.x < 200 || player.snipRect.x > 300 )
+	    			{
+	    				player.snipRect.x = 200;
+					}
+				}
+				else if( keySIsDown && keyDIsDown )
+				{
+					player.presentedRect.x += player.speed;
+					player.presentedRect.y += player.speed;
+					player.snipRect.x += 100;
+					if( player.snipRect.x < 0 || player.snipRect.x > 100 )
+	    			{
+	    				player.snipRect.x = 0;
+					}
+				}
+				else if( keySIsDown && keyAIsDown )
+				{
+					player.presentedRect.x -= player.speed;
+					player.presentedRect.y += player.speed;
+					player.snipRect.x += 100;
+					if( player.snipRect.x < 0 || player.snipRect.x > 100 )
+	    			{
+	    				player.snipRect.x = 0;
+					}
+				}
+				else if(keyUPIsDown && keyLEFTIsDown )
+				{
+
+					V[i].presentedRect.x = player.presentedRect.x;
+					V[i].presentedRect.y = player.presentedRect.y; 
+					V[i].direction = 5;
+					V[i].activate = 1;
+
+					i = i+1;
+
+					if(i>49){
+						i = 0;
+					}
+	
+				}
+				else if(keyUPIsDown && keyRIGHTIsDown )
+				{
+
+					V[i].presentedRect.x = player.presentedRect.x;
+					V[i].presentedRect.y = player.presentedRect.y; 
+					V[i].direction = 6;
+					V[i].activate = 1;
+
+					i = i+1;
+
+					if(i>49){
+						i = 0;
+					}
+	
+				}
+				else if(keyDOWNIsDown && keyLEFTIsDown )
+				{
+
+					V[i].presentedRect.x = player.presentedRect.x;
+					V[i].presentedRect.y = player.presentedRect.y; 
+					V[i].direction = 7;
+					V[i].activate = 1;
+
+					i = i+1;
+
+					if(i>49){
+						i = 0;
+					}
+	
+				}
+				else if(keyDOWNIsDown && keyRIGHTIsDown )
+				{
+
+					V[i].presentedRect.x = player.presentedRect.x;
+					V[i].presentedRect.y = player.presentedRect.y; 
+					V[i].direction = 8;
+					V[i].activate = 1;
+
+					i = i+1;
+
+					if(i>49){
+						i = 0;
+					}
+	
+				}
+				else if( keyUPIsDown )
+				{
+
+					V[i].presentedRect.x = player.presentedRect.x;
+					V[i].presentedRect.y = player.presentedRect.y; 
+					V[i].direction = 1;
+					V[i].activate = 1;
+
+					i = i+1;
+
+					if(i>49){
+						i = 0;
+					}
+	
+				}
+				else if( keyDOWNIsDown )
+				{
+
+					V[i].presentedRect.x = player.presentedRect.x;
+					V[i].presentedRect.y = player.presentedRect.y; 
+					V[i].direction = 2;
+					V[i].activate = 1;
+
+					i = i+1;
+
+					if(i>49){
+						i = 0;
+					}
+	
+				}
+				else if( keyLEFTIsDown )
+				{
+
+					V[i].presentedRect.x = player.presentedRect.x;
+					V[i].presentedRect.y = player.presentedRect.y; 
+					V[i].direction = 3;
+					V[i].activate = 1;
+
+					i = i+1;
+
+					if(i>49){
+						i = 0;
+					}
+	
+				}
+				else if(keyRIGHTIsDown)
+				{
+
+					V[i].presentedRect.x = player.presentedRect.x;
+					V[i].presentedRect.y = player.presentedRect.y; 
+					V[i].direction = 4;
+					V[i].activate = 1;
+
+					i = i+1;
+
+					if(i>49){
+						i = 0;
+					}
+	
+				} 
+				else
+				{
+					
+           			switch( e.key.keysym.sym )
+        			{
+						case SDLK_d:	
+        					player.presentedRect.x += player.speed;
+        					player.snipRect.x += 100;
+        					if( player.snipRect.x < 1400 || player.snipRect.x > 1500 )
+			    			{
+			    				player.snipRect.x = 1400;
+							}
+							break;
+	
+						case SDLK_a: 
+							player.presentedRect.x -= player.speed;
+							player.snipRect.x -= 100;
+							if( player.snipRect.x < 1800 || player.snipRect.x > 1900 )
+			    			{
+			    				player.snipRect.x = 1800;
+							}
+							break;
+	
+						case SDLK_w:
+							player.presentedRect.y -= player.speed;
+							player.snipRect.x -= 100;
+							if( player.snipRect.x < 200 || player.snipRect.x > 300 )
+			    			{
+			    				player.snipRect.x = 200;
+							}
+							break;
+	
+						case SDLK_s:
+							player.presentedRect.y += player.speed;
+							player.snipRect.x += 100;
+							if( player.snipRect.x < 0 || player.snipRect.x > 100 )
+			    			{
+			    				player.snipRect.x = 0;
+							}
+							break;
+						
+						//case SDLK_RETURN:
+						//	l_gameState = -1;                                     
+						//	break;
+        			}
+    			}	
+    		}
+    		
+    		else
+			{
+    			player.snipRect.x += 80;
+    			if( player.snipRect.x < 400 || player.snipRect.x >700 )
+    			{
+    				player.snipRect.x = 400;
+				}
+			}
+    	}
+	}
+	
+	// Libera imagens carregadas
+	SDL_DestroyTexture( player.sprite );
+	SDL_DestroyTexture( firstMap.texture );
+	player.sprite = NULL;
+	firstMap.texture = NULL;
+	
+	// Limpa o renderizador
+	SDL_RenderClear( l_renderer );	
+	
+	return l_gameState;
+}
+
--- main.c
+++ main.c
@@ -1,102 +1,115 @@
 #include <stdio.h>
-#include <string.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
+#include "loaders_and_effects.h"
+#include "opening.h"
+#include "title_screen.h"
+#include "menu.h"
+#include "gameplay.h"
 
 // Dimensoes constantes da tela
 const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
+const int SCREEN_HEIGHT = 600;	
+
+// Cria os estados do jogo
+int STATE_OPENING = 0;
+int STATE_TITLE_SCREEN = 1;
+int STATE_MAIN_MENU = 2;
+int STATE_GAMEPLAY = 3;
+int STATE_HIGHSCORE = 4;
+int STATE_CREDITS = 5;
+int STATE_OPTIONS = 6;
+int STATE_PAUSE = 7;
+
+// Iniciliza o estado do jogo como tela inicial
+int g_gameState = 2;
+
+// A janela do jogo
+SDL_Window* g_window = NULL;
+
+// O renderizador da janela
+SDL_Renderer* g_renderer = NULL;
 
 // Inicializa o SDL e cria janela
 void init_boot_game();
 
-// Carrega imagem PNG como textura
-void load_Texture( char* l_Path );
-
-//Aplica e atualiza a textura
-void apply_Texture ();
-
 // Limpa memoria e fecha o SDL e o jogo
 int close_game();
 
-// A janela do jogo
-SDL_Window* g_Window = NULL;
-
-// O renderizador da janela
-SDL_Renderer* g_Renderer = NULL;
-
-// Textura atual sendo apresentada
-SDL_Texture* g_Texture = NULL;
-
-/*===================================================================================================================================================================================*/
-/*=====================================================================SEPARANDO=====================================================================================================*/
-/*===================================================================================================================================================================================*/
+/*========================================================================================================================================
+  ==================================================================SEPARANDO=============================================================
+  ========================================================================================================================================*/
 
 void init_boot_game()
 {
 	// Inicializa o SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	if( SDL_Init( SDL_INIT_VIDEO || SDL_INIT_AUDIO ) < 0 )
 	{
+		// Apresenta o erro causado 
 		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+		
+		// Fechará o jogo ao passar pelo loop principal
+		g_gameState = 666;
 	}
 	
 	else
 	{
 		//Cria a janela
-		g_Window = SDL_CreateWindow( "DSS_Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( g_Window == NULL )
+		g_window = SDL_CreateWindow( "DSS Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( g_window == NULL )
 		{
 			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+			g_gameState = 666;
 		}
 		
 		else
 		{
 			// Cria renderizador para a janela
-			g_Renderer = SDL_CreateRenderer( g_Window, -1, SDL_RENDERER_ACCELERATED );
-			if( g_Renderer == NULL )
+			g_renderer = SDL_CreateRenderer( g_window, -1, SDL_RENDERER_ACCELERATED );
+			if( g_renderer == NULL )
 			{
 				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+				g_gameState = 666;
 			}
 			
 			else
 			{
 				//Inicializa a cor do renderizador
-				SDL_SetRenderDrawColor( g_Renderer, 0xFF, 0xFF, 0xFF, 0xFF );
+				SDL_SetRenderDrawColor( g_renderer, 255, 0, 0, 0 );
 				
 				// Inicializa o carregamento de PNG
 				int imgFlags = IMG_INIT_PNG;
 				if( !( IMG_Init( imgFlags ) & imgFlags ) )
 				{
 					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+					g_gameState = 666;
 				}
+				
+				else
+				{
+					// Inicializa o carregamento de sons e musicas
+	                if( Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 ) < 0 )
+	                {
+	                    printf( "SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError() );
+	                    g_gameState = 666;
+	                }
+	        	}
 			}
 		}
 	}
 }
 
-int main(int argc, char *argv[])
-{    	
+int main( int argc, char *argv[] )
+{  	
 	// Iniciando o jogo
-	init_boot_game() ;
-	
-	// Flag do loop principal
-	int l_quit_game = 0;
-		
-	// Carrega a imagem PNG da empresa em textura, depois aplica a textura e a atualiza a tela
-	load_Texture( "empresa.png" );
-	apply_Texture ();
-	SDL_Delay( 3000 );
-		
-	// Carrega a imagem PNG dos creditos ao SDL 2.0 em textura, depois aplica a textura e a atualiza a tela
-	load_Texture( "sdlcredits.png" );
-	apply_Texture ();
-	SDL_Delay( 3000 );
+	init_boot_game();
 
 	// Cuidador de evento
 	SDL_Event e;
 
-	// Enquanto a aplicaÃ§Ã£o esta rodando
-	while( l_quit_game != 1 )
+	// Enquanto a aplicação esta rodando
+	while( g_gameState != 666 )
 	{
 		// Cuida dos eventos na fila
 		while( SDL_PollEvent( &e ) != 0 )
@@ -104,69 +117,146 @@ int main(int argc, char *argv[])
 			// Usuario deseja sair
 			if( e.type == SDL_QUIT )
 			{
-				l_quit_game = 1;
+				g_gameState = -1;
 			}
 		}
-			
-		// Carrega a imagem PNG da tela inicial em textura, depois aplica a textura e a atualiza a tela
-		load_Texture( "titlescreen.png" );
-		apply_Texture ();
+
+		// Muda para a lógica de cada g_gameState
+		switch ( g_gameState )
+		{
+			case 0:
+				g_gameState = opening_logic( g_window, g_renderer );
+				break;
+	
+			case 1:
+				g_gameState = title_screen_logic( g_window, g_renderer );
+				break;
+
+			case 2: 
+				g_gameState = main_menu_logic( g_window, g_renderer ); 
+				break; 
+
+			case 3:
+				g_gameState = gameplay_logic( g_window, g_renderer ); 
+				break;
+
+			case 6:
+				//options_logic( g_window, g_renderer ); 
+				g_gameState = -1;
+				break;
+				
+			case 4:
+				//highscore_logic( g_window, g_renderer );
+				g_gameState = -1;
+				break;
+				
+			case 5:
+				//credits_logic( g_window, g_renderer );
+				g_gameState = -1;
+				break;		
+
+			case 7:
+				//pause_logic( g_window, g_renderer );
+				g_gameState = -1;
+				break;
+
+			default:
+				g_gameState = 666;
+				break; 
+		}
 	}
 	
+	// Destroi a janela e o renderizador
+	SDL_DestroyRenderer( g_renderer );
+	SDL_DestroyWindow( g_window );
+	g_window = NULL;
+	g_renderer = NULL;
+
 	return close_game();
 }
 
-void load_Texture( char* l_Path )
+/*
+int gameplay_logic( int argc, char *argv[] )
 {
-	//Imagem PNG em surface e textura
-	SDL_Surface* l_loadedSurface = NULL;
+	free_music_sfx( 'b' );
+	free_texture();
 
-	// Carrega a imagem de um diretorio
-	l_loadedSurface = IMG_Load( l_Path );
-	if( l_loadedSurface == NULL )
-	{
-		printf( "Unable to load image %s! SDL_image Error: %s\n", l_Path, IMG_GetError() );
-	}
+	// "Velocidade" do sprites
+	float velx = 2.5;	
 	
-	else
-	{
-		// Gera textura atraves da surface
-        g_Texture = SDL_CreateTextureFromSurface( g_Renderer, l_loadedSurface );
-		if( g_Texture == NULL )
+	load_Texture( "mapa.png" ); 
+	SDL_RenderClear( g_renderer );
+    SDL_RenderCopy( g_renderer, g_texture, NULL, NULL );
+    
+    SDL_Event e;
+    
+    // Area que sera apresentada da imagem fonte
+	SDL_Rect l_srcRect;
+	l_srcRect.x = 0;
+	l_srcRect.y = 0;
+    l_srcRect.w = 80;
+    l_srcRect.h = 80;    
+    
+    // Area onde a imagem fonte sera aplicada
+	SDL_Rect l_dstRect;	
+	l_dstRect.x = SCREEN_WIDTH/2;
+  	l_dstRect.y = SCREEN_HEIGHT/2;
+   	l_dstRect.w = 80;
+   	l_dstRect.h = 80;
+	
+	load_Texture( "sprites/sombra100-60.png" );
+	SDL_RenderCopy( g_renderer, g_texture, &l_srcRect, &l_dstRect );
+	SDL_RenderPresent( g_renderer );
+
+	 while( g_gameState == 3 )
+	 {
+	 	while( SDL_PollEvent( &e ) != 0 )
 		{
-			printf( "Unable to create texture from %s! SDL Error: %s\n", l_Path, SDL_GetError() );
-		}
 
-		// Se livra da surface que nao sera usada
-		SDL_FreeSurface( l_loadedSurface );
+			free_texture();
+	        SDL_RenderClear( g_renderer );		
+			load_Texture( "mapa.png" );
+	        SDL_RenderCopy( g_renderer, g_texture, NULL, NULL );
+	        free_texture();
+			load_Texture( "sprites/sombra100-60.png" );
+	        SDL_RenderCopy( g_renderer, g_texture, &l_srcRect, &l_dstRect );
+	       	SDL_RenderPresent( g_renderer );
+		
+	    	if( e.type == SDL_KEYDOWN )
+			{
+	    		switch( e.key.keysym.sym )
+		   		{
+					case SDLK_RIGHT:
+	        			l_dstRect.x += velx;
+						break;
+	
+					case SDLK_LEFT: 
+						l_dstRect.x -= velx;
+						break;
+	
+					case SDLK_UP:
+						l_dstRect.y -= velx;
+						//l_srcRect.x -= l_srcRect.w;
+						break;
+	
+					case SDLK_DOWN:
+						l_dstRect.y += velx;
+						//l_srcRect.x -= l_srcRect.w; 
+						break;
+	
+					case SDLK_RETURN:
+						g_gameState = 5;                                     
+						break;
+           		}
+        	}
+        }
 	}
-}
-
-void apply_Texture ()
-{
-	// Limpa tela
-	SDL_RenderClear( g_Renderer );
-
-	// Renderiza a textura a tela
-	SDL_RenderCopy( g_Renderer, g_Texture, NULL, NULL );
-
-	// Atualiza a tela
-	SDL_RenderPresent( g_Renderer );
-}
+} */
 
 int close_game()
 {
-	// Libera imagens carregadas
-	SDL_DestroyTexture( g_Texture );
-	g_Texture = NULL;
-
-	// Destroi a janela
-	SDL_DestroyRenderer( g_Renderer );
-	SDL_DestroyWindow( g_Window );
-	g_Window = NULL;
-	g_Renderer = NULL;
-
 	// Sai dos subsistemas do SDL
+	Mix_Quit();
 	IMG_Quit();
 	SDL_Quit();
 	
